92c1ff7a8217b898a132a13a4f3131fca2f2e588
Californication <miroslav_mm@ukr.net>
2013-03-18 22:44:21 +0200

    new improved Power HAL

--- power/power_iprj.c
+++ power/power_iprj.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (c) 2012 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,29 +20,56 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-#define LOG_TAG "I-Project PowerHAL"
+#define LOG_TAG "CM PowerHAL"
 #include <utils/Log.h>
 
 #include <hardware/hardware.h>
 #include <hardware/power.h>
 
-// Uncomment this if we want to default to the ondemand governor
-#define USING_ONDEMAND
-
-#define BOOSTPULSE_INTERACTIVE_PATH "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
-#define BOOSTPULSE_ONDEMAND_PATH "/sys/devices/system/cpu/cpufreq/ondemand/boostpulse"
-#ifdef USING_ONDEMAND
-#define SAMPLING_RATE_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemand/sampling_rate"
+#define SCALING_GOVERNOR_PATH "/sys/devices/system/cpu/cpu0/cpufreq/scaling_governor"
+#define BOOSTPULSE_ONDEMAND "/sys/devices/system/cpu/cpufreq/ondemand/boostpulse"
+#define BOOSTPULSE_INTERACTIVE "/sys/devices/system/cpu/cpufreq/interactive/boostpulse"
 #define SAMPLING_RATE_SCREEN_ON "50000"
 #define SAMPLING_RATE_SCREEN_OFF "500000"
-#endif
+#define TIMER_RATE_SCREEN_ON "30000"
+#define TIMER_RATE_SCREEN_OFF "500000"
 
-struct iprj_power_module {
+struct cm_power_module {
     struct power_module base;
     pthread_mutex_t lock;
     int boostpulse_fd;
     int boostpulse_warned;
 };
+
+static char governor[20];
+
+static int sysfs_read(char *path, char *s, int num_bytes)
+{
+    char buf[80];
+    int count;
+    int ret = 0;
+    int fd = open(path, O_RDONLY);
+
+    if (fd < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error opening %s: %s\n", path, buf);
+
+        return -1;
+    }
+
+    if ((count = read(fd, s, num_bytes - 1)) < 0) {
+        strerror_r(errno, buf, sizeof(buf));
+        ALOGE("Error writing to %s: %s\n", path, buf);
+
+        ret = -1;
+    } else {
+        s[count] = '\0';
+    }
+
+    close(fd);
+
+    return ret;
+}
 
 static void sysfs_write(char *path, char *s)
 {
@@ -64,93 +92,111 @@ static void sysfs_write(char *path, char *s)
     close(fd);
 }
 
-static int boostpulse_open(struct iprj_power_module *iprj)
+static int get_scaling_governor() {
+    if (sysfs_read(SCALING_GOVERNOR_PATH, governor,
+                sizeof(governor)) == -1) {
+        return -1;
+    } else {
+        // Strip newline at the end.
+        int len = strlen(governor);
+
+        len--;
+
+        while (len >= 0 && (governor[len] == '\n' || governor[len] == '\r'))
+            governor[len--] = '\0';
+    }
+
+    return 0;
+}
+
+static void cm_power_set_interactive(struct power_module *module, int on)
+{
+    if (strncmp(governor, "ondemand", 8) == 0)
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/sampling_rate",
+                on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
+    else if (strncmp(governor, "interactive", 11) == 0)
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
+                on ? TIMER_RATE_SCREEN_ON : TIMER_RATE_SCREEN_OFF);
+}
+
+
+static void configure_governor()
+{
+    cm_power_set_interactive(NULL, 1);
+
+    if (strncmp(governor, "ondemand", 8) == 0) {
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/up_threshold", "90");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/io_is_busy", "1");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor", "4");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/ondemand/down_differential", "10");
+
+    } else if (strncmp(governor, "interactive", 11) == 0) {
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time", "90000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq", "1134000");
+        sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay", "30000");
+    }
+}
+
+static int boostpulse_open(struct cm_power_module *cm)
 {
     char buf[80];
 
-    pthread_mutex_lock(&iprj->lock);
+    pthread_mutex_lock(&cm->lock);
 
-    if (iprj->boostpulse_fd < 0) {
-        iprj->boostpulse_fd = open(BOOSTPULSE_ONDEMAND_PATH, O_WRONLY);
-        if (iprj->boostpulse_fd < 0) {
-            iprj->boostpulse_fd = open(BOOSTPULSE_INTERACTIVE_PATH, O_WRONLY);
+    if (cm->boostpulse_fd < 0) {
+        if (get_scaling_governor() < 0) {
+            ALOGE("Can't read scaling governor.");
+            cm->boostpulse_warned = 1;
+        } else {
+            if (strncmp(governor, "ondemand", 8) == 0)
+                cm->boostpulse_fd = open(BOOSTPULSE_ONDEMAND, O_WRONLY);
+            else if (strncmp(governor, "interactive", 11) == 0)
+                cm->boostpulse_fd = open(BOOSTPULSE_INTERACTIVE, O_WRONLY);
 
-            if (iprj->boostpulse_fd < 0 && !iprj->boostpulse_warned) {
+            if (cm->boostpulse_fd < 0 && !cm->boostpulse_warned) {
                 strerror_r(errno, buf, sizeof(buf));
-                ALOGE("Error opening boostpulse: %s\n", buf);
-                iprj->boostpulse_warned = 1;
+                ALOGV("Error opening boostpulse: %s\n", buf);
+                cm->boostpulse_warned = 1;
+            } else if (cm->boostpulse_fd > 0) {
+                configure_governor();
+                ALOGD("Opened %s boostpulse interface", governor);
             }
         }
     }
 
-    pthread_mutex_unlock(&iprj->lock);
-    return iprj->boostpulse_fd;
+    pthread_mutex_unlock(&cm->lock);
+    return cm->boostpulse_fd;
 }
 
-#ifdef USING_ONDEMAND
-
-static void iprj_power_init(struct power_module *module)
-{
-    sysfs_write(SAMPLING_RATE_ONDEMAND, SAMPLING_RATE_SCREEN_ON);
-}
-
-static void iprj_power_set_interactive(struct power_module *module, int on)
-{
-    sysfs_write(SAMPLING_RATE_ONDEMAND,
-            on ? SAMPLING_RATE_SCREEN_ON : SAMPLING_RATE_SCREEN_OFF);
-}
-
-#else // interactive
-
-static void iprj_power_init(struct power_module *module)
-{
-    /*
-     * cpufreq interactive governor: timer 20ms, min sample 60ms,
-     * hispeed 700MHz at load 50%.
-     */
-
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/timer_rate",
-                "20000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/min_sample_time",
-                "60000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/hispeed_freq",
-                "702000");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load",
-                "50");
-    sysfs_write("/sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay",
-                "100000");
-}
-
-static void iprj_power_set_interactive(struct power_module *module, int on)
-{
-    /*
-     * Lower maximum frequency when screen is off.  CPU 0 and 1 share a
-     * cpufreq policy.
-     */
-
-    sysfs_write("/sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq",
-                on ? "1512000" : "702000");
-}
-
-#endif
-
-static void iprj_power_hint(struct power_module *module, power_hint_t hint,
+static void cm_power_hint(struct power_module *module, power_hint_t hint,
                             void *data)
 {
-    struct iprj_power_module *iprj = (struct iprj_power_module *) module;
+    struct cm_power_module *cm = (struct cm_power_module *) module;
     char buf[80];
     int len;
+    int duration = 1;
 
     switch (hint) {
     case POWER_HINT_INTERACTION:
-        if (boostpulse_open(iprj) >= 0) {
-	    len = write(iprj->boostpulse_fd, "1", 1);
+    case POWER_HINT_CPU_BOOST:
+        if (boostpulse_open(cm) >= 0) {
+            if (data != NULL)
+                duration = (int) data;
 
-	    if (len < 0) {
-	        strerror_r(errno, buf, sizeof(buf));
-		ALOGE("Error writing to boostpulse: %s\n", buf);
-	    }
-	}
+            snprintf(buf, sizeof(buf), "%d", duration);
+            len = write(cm->boostpulse_fd, buf, strlen(buf));
+
+            if (len < 0) {
+                strerror_r(errno, buf, sizeof(buf));
+	            ALOGE("Error writing to boostpulse: %s\n", buf);
+
+                pthread_mutex_lock(&cm->lock);
+                close(cm->boostpulse_fd);
+                cm->boostpulse_fd = -1;
+                cm->boostpulse_warned = 0;
+                pthread_mutex_unlock(&cm->lock);
+            }
+        }
         break;
 
     case POWER_HINT_VSYNC:
@@ -161,25 +207,30 @@ static void iprj_power_hint(struct power_module *module, power_hint_t hint,
     }
 }
 
+static void cm_power_init(struct power_module *module)
+{
+    get_scaling_governor();
+    configure_governor();
+}
+
 static struct hw_module_methods_t power_module_methods = {
     .open = NULL,
 };
 
-struct iprj_power_module HAL_MODULE_INFO_SYM = {
+struct cm_power_module HAL_MODULE_INFO_SYM = {
     base: {
         common: {
             tag: HARDWARE_MODULE_TAG,
             module_api_version: POWER_MODULE_API_VERSION_0_2,
             hal_api_version: HARDWARE_HAL_API_VERSION,
             id: POWER_HARDWARE_MODULE_ID,
-            name: "I-Project Power HAL",
-            author: "The Android Open Source Project",
+            name: "CM Power HAL",
+            author: "The CyanogenMod Project",
             methods: &power_module_methods,
         },
-
-       init: iprj_power_init,
-       setInteractive: iprj_power_set_interactive,
-       powerHint: iprj_power_hint,
+       init: cm_power_init,
+       setInteractive: cm_power_set_interactive,
+       powerHint: cm_power_hint,
     },
 
     lock: PTHREAD_MUTEX_INITIALIZER,
